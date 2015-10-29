d68b552825ab5f3036eea0f1af1e562f307b1bef
Wacky <ne260183@senshu-u.jp>
2015-10-29 19:34:42 +0900

    DM機能
    とりあえず新規ファイルだけ
    プルリクエラーが起きてるよ

---
+++ app/webroot/js/post.js
@@ -0,0 +1,9 @@
+flag =false;
+var index;
+function allChange(){
+	flag = !flag; // trueとfalseの切り替え ! 否定演算子
+	var elem = document.getElementsByName("select[]");
+	for(index = 0; index < elem.length; index++){
+		elem[index].checked = flag;
+	}
+}
